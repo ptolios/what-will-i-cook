f976382caf3f7d861af2bcc079c9965cb6033692
Panos Tolios <panostolios@gmail.com>
2019-11-05 21:49:13 +0000

    Put dark overlay on background image

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
           class="fill-height remove-top-margin"
           src="https://farm5.staticflickr.com/4137/4788033666_6569900768_b.jpg"
           position="top center"
+          gradient="rgba(0,0,0,.4), rgba(0,0,0,.4)"
           height="100vh"
         ></v-img>
       </v-container>
@@ -33,8 +34,13 @@ export default Vue.extend({
 .remove-top-margin {
   margin-top: -56px;
 }
+
 v-content {
   margin-top: 0;
   padding-top: 0;
+}
+
+.fill-hght {
+  height: 100%;
 }
 </style>
