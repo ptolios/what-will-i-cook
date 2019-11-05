111318b078da3e086f36f1343853982c9ad33ef0
Panos Tolios <panostolios@gmail.com>
2019-11-05 09:27:16 +0000

    Configure light theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,5 +6,15 @@ Vue.use(Vuetify);
 export default new Vuetify({
   icons: {
     iconfont: "mdi"
+  },
+  theme: {
+    themes: {
+      //     dark: true,
+      light: {
+        primary: "#FFC501",
+        secondary: "#57C701",
+        accent: "#7FC613"
+      }
+    }
   }
 });
