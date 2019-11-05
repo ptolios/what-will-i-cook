4be8523f9ec909ae1cd518f22a4e002df996f57a
Panos Tolios <panostolios@gmail.com>
2019-11-05 09:38:10 +0000

    Setup Header compomnent on mobile

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar class="transparent" app>
+    <v-app-bar-nav-icon color="primary"></v-app-bar-nav-icon>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
