eab002f20b9a78f73cbf0c49fc3b2999c1106b1c
Panos Tolios <panostolios@gmail.com>
2019-11-06 11:30:32 +0000

    Add search box on landing page

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
     <v-content>
       <v-container class="fill-height pa-0" fluid>
         <v-img
-          class="fill-height remove-top-margin"
+          class="fill-height remove-top-margin align-center"
           src="https://farm5.staticflickr.com/4137/4788033666_6569900768_b.jpg"
           position="top center"
           gradient="rgba(0,0,0,.4), rgba(0,0,0,.4)"
           height="100vh"
-        ></v-img>
+        >
+          <v-row align="center" class="fill-height">
+            <v-col class="mx-12">
+              <SearchBox></SearchBox>
+            </v-col>
+          </v-row>
+        </v-img>
       </v-container>
     </v-content>
   </v-app>
@@ -17,12 +23,14 @@
 
 <script lang="ts">
 import Vue from "vue";
-import Header from "./components/Header.vue";
+import Header from "@/components/Header.vue";
+import SearchBox from "@/components/SearchBox.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    Header
+    Header,
+    SearchBox
   },
   data: () => ({
     //
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-text-field
+    class="align-self-center"
+    color="secondary"
+    background-color="ingredients"
+    rounded
+    single-line
+    filled
+    label="Search"
+    append-icon="mdi-search"
+  ></v-text-field>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
