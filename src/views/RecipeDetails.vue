249b510aa86c52b3e4c98b0c7cd1a405c427e333
Panos Tolios <panostolios@gmail.com>
2019-12-11 10:22:41 +0000

    Add Recipe Overview section

--- src/views/RecipeDetails.vue
+++ src/views/RecipeDetails.vue
@@ -1,23 +1,61 @@
 <template>
   <v-container>
-    <v-row> </v-row>
+    <v-row v-if="loading">
+      <v-progress-circular
+        indeterminate
+        color="secondary"
+      ></v-progress-circular>
+    </v-row>
+    <v-row v-else-if="error" class="mt-5">
+      <v-card>
+        {{ error }}
+      </v-card>
+    </v-row>
+    <v-row v-else>
+      <v-col>
+        <recipe-overview :imageUrl="image">
+          <template v-slot:title>
+            {{ recipe.title }}
+          </template>
+          <template v-slot:preparation-minutes>
+            {{ recipe.preparationMinutes }}
+          </template>
+          <template v-slot:cooking-minutes>
+            {{ recipe.cookingMinutes }}
+          </template>
+          <template v-slot:servings>
+            {{ recipe.servings }}
+          </template>
+        </recipe-overview>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import { fetchRecipe } from "@/api-utils/spoonacular-api";
+import RecipeOverview from "@/components/Recipe/RecipeOverview.vue";
 
 const apiKey = process.env.VUE_APP_SPOONACULAR_API_KEY || "";
 const baseURL = process.env.VUE_APP_SPOONACULAR_API_URL || "";
 const config = { apiKey, baseURL };
 
 export default {
+  components: {
+    RecipeOverview
+  },
   data() {
     return {
       recipe: {},
       loading: true,
+      // image: "https://spoonacular.com/recipeImages/218768-556x370.jpg"
       error: false
     };
+  },
+  computed: {
+    image() {
+      return this.recipe.image;
+    }
   },
   mounted() {
     const id = this.$route.params.id;
@@ -25,6 +63,7 @@ export default {
       .then(response => {
         const { data } = response;
         this.recipe = data;
+        console.log(data);
         this.loading = false;
       })
       .catch(error => {
