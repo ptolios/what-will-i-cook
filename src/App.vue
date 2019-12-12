ff75cc2964eadff368c79320fedf97a1865eb9ae
Panos Tolios <panostolios@gmail.com>
2019-12-12 22:13:50 +0000

    Fix App.vue to be the root compoent again

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <v-app>
-    <Header />
-    <router-view></router-view>
+    <app-header />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
     <!-- TODO: change header background from transparent to white when scrolling down -->
   </v-app>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import AppHeader from "@/components/Header.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    AppHeader
+  }
+});
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,39 @@
 <template>
-  <v-content>
-    <v-container class="height100 pa-0 remove-top-margin" fluid>
-      <div class="d-flex flex-column back fill-height">
-        <div class="app-title d-flex align-center justify-center">
-          <app-title class="h1 primary--text text-center font-weight-medium"
-            >What will I cook today?</app-title
-          >
-        </div>
-        <search-form></search-form>
-        <div class="pa-4">
-          <div class="text-center">
-            <h2 class="primary--text">- or -</h2>
-          </div>
-        </div>
-        <div class="pa-4">
-          <div class="text-center">
-            <app-btn
-              @btnclicked="$vuetify.goTo($refs.recipes)"
-              color="primary"
-              class="font-weight-bold"
-              >Surprise Me!</app-btn
-            >
-          </div>
+  <v-container class="height100 pa-0 remove-top-margin" fluid>
+    <div class="d-flex flex-column back fill-height">
+      <div class="app-title d-flex align-center justify-center">
+        <app-title class="h1 primary--text text-center font-weight-medium"
+          >What will I cook today?</app-title
+        >
+      </div>
+      <search-form></search-form>
+      <div class="pa-4">
+        <div class="text-center">
+          <h2 class="primary--text">- or -</h2>
         </div>
       </div>
-      <recipes-grid ref="recipes"></recipes-grid>
-    </v-container>
-  </v-content>
+      <div class="pa-4">
+        <div class="text-center">
+          <app-btn
+            @btnclicked="$vuetify.goTo($refs.recipes)"
+            color="primary"
+            class="font-weight-bold"
+            >Surprise Me!</app-btn
+          >
+        </div>
+      </div>
+    </div>
+    <recipes-grid ref="recipes"></recipes-grid>
+  </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import AppTitle from "@/components/AppTitle.vue";
 import AppBtn from "@/components/AppBtn.vue";
 import RecipesGrid from "@/components/RecipesGrid.vue";
 import SearchForm from "@/components/SearchForm.vue";
 
-export default Vue.extend({
-  name: "App",
+export default {
   components: {
     AppTitle,
     AppBtn,
@@ -49,7 +45,7 @@ export default Vue.extend({
       //
     };
   }
-});
+};
 </script>
 
 <style>
