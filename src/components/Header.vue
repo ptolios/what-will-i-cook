0884fec01d69613b65bef6e4e0a7be18f2e487aa
Panos Tolios <panostolios@gmail.com>
2019-12-13 08:15:31 +0000

    Hide Header and show it when scrolled

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar class="white" app>
+  <v-app-bar class="white" app invertedScroll>
     <v-app-bar-nav-icon color="primary"></v-app-bar-nav-icon>
   </v-app-bar>
 </template>
