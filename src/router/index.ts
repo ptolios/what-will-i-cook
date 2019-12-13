53404dd14842a1b24b4de01c62745867e6203561
Panos Tolios <panostolios@gmail.com>
2019-12-13 12:30:41 +0000

    Rename RecipeDetails to RecipePage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
-import RecipeDetails from "@/views/RecipeDetails.vue";
+import RecipePage from "@/views/RecipePage.vue";
 
 Vue.use(VueRouter);
 
@@ -14,7 +14,7 @@ const routes = [
   {
     path: "/recipe/:id",
     name: "recipe",
-    component: RecipeDetails
+    component: RecipePage
   },
   {
     path: "/about",
