83b311abb324cc6161a9cabf2df703edefc777f4
Panos Tolios <panostolios@gmail.com>
2019-12-18 21:01:05 +0000

    Create custom svg component

---
+++ src/components/common/SvgIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-avatar :size="size">
+    <svg :fill="fill">
+      <use :xlink:href="href + '#' + svgid"></use>
+    </svg>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  props: ["svgid", "size", "fill", "href"]
+};
+</script>
