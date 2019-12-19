84fdcf5f556366d03ad40558bef34784f2133d4e
Panos Tolios <panostolios@gmail.com>
2019-12-19 14:47:08 +0000

    Minor layout changes on recipe cards

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container>
     <v-row class="flex-grow-0" justify="center" align="baseline">
-      <v-col cols="12">
-        <div class="my-2">
+      <v-col cols="12" class="py-1">
+        <div class="my-0">
           <h1 class="text-center primary--text font-weight-black">
             Search Results
           </h1>
         </div>
       </v-col>
-      <v-col class="my-1" cols="12">
+      <v-col class="py-1" cols="12">
         <h4 class="text-center secondary--text font-weight-regular">
           You are searching for:
           <span class="font-italic">{{ query }}</span>
