6c7de0a342cc7c028474ded997eb8da3b13bd728
Panos Tolios <panostolios@gmail.com>
2020-01-10 20:54:35 +0000

    Add e2e test for the homepage

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,28 +1,20 @@
 /// <reference types="Cypress" />
 describe("Home page", function () {
-  it("Contains a header", function () {
-    cy.visit("https://wwict.panostolios.dev")
+  it('is set up properly', function () {
+    cy.visit('/')
     cy.get("header")
-  })
-  it("Contains a footer", function () {
     cy.get("footer")
-  })
-  it("Contains the correct title", function () {
     cy.contains("What will I cook today?")
-  })
-  it("Contains a search box", function () {
     cy.get(".v-form")
       .get("input")
       .get("button").contains("search")
-  })
-  it("Contains a 'Surprise Me!' button", function () {
-    cy.get("button").contains("Surprise Me!")
+    cy.contains("Surprise Me!")
   })
 })
 
 describe("'Surprise Me!' button", function () {
-  it("Navigates to a random recipe", function () {
+  it("navigates to a random recipe page", function () {
     cy.contains("Surprise Me!").click()
-    cy.url().should("include", "/recipe/")
+    cy.url().should("match", /recipe\//)
   })
 })
