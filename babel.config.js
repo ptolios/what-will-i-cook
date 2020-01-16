639cefa46c42fc2ca15026cd6cc236b3f9ad5c65
Panos Tolios <panostolios@gmail.com>
2020-01-16 13:05:20 +0000

    Setup jest the right way
    Also upgrade npm packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  "plugins": ["@babel/plugin-transform-typescript"],
+  presets: [
+    "@vue/cli-plugin-babel/preset",
+    "@babel/preset-typescript"
+  ]
 };
