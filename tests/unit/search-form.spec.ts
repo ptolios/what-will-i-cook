7921551b75090a9b8a22f7ba82caf95d3538ff70
Panos Tolios <panostolios@gmail.com>
2020-01-18 21:36:48 +0000

    Add tests for SearchForm

---
+++ tests/unit/search-form.spec.ts
@@ -0,0 +1,43 @@
+// import Vue from "vue"
+import { shallowMount, createLocalVue } from "@vue/test-utils"
+import VueRouter from "vue-router"
+import Vuetify from "vuetify"
+import SearchForm from "@/components/common/search/SearchForm.vue"
+
+const localVue = createLocalVue()
+
+describe("SearchForm", () => {
+  const label: String = "Search by ingredient, cuisine, meal type etc."
+  const inputText = "Eggs salad mexican"
+
+  localVue.use(Vuetify)
+  localVue.use(VueRouter)
+
+  const routes = [{ path: "/search", name: "search" }]
+
+  const router = new VueRouter({ routes })
+  const data = { query: inputText }
+
+  let wrapper = shallowMount(SearchForm, {
+    router,
+    propsData: { label }
+  })
+
+  it("sets the correct props", () => {
+    expect(wrapper.props("label")).toBe(label)
+  })
+  it("sets the correct data", () => {
+    wrapper.setData(data)
+    const searchform = wrapper.find("[data-test='searchform']")
+    const searchbox = wrapper.find("[data-test='searchbox']")
+    expect(searchbox.attributes("label")).toBe(label)
+    expect(searchform.vm.$data.query).toBe(inputText)
+  })
+  it("submitQuery event emits the correct payload", () => {
+    wrapper.setData(data)
+    wrapper.vm.$emit("submitQuery", inputText)
+    expect(wrapper.emitted().submitQuery).toBeTruthy
+    expect(wrapper.emitted().submitQuery.length).toBe(1)
+    expect(wrapper.emitted().submitQuery[0]).toEqual([inputText])
+  })
+})
