494795064ab5268c9674624239d30274ddedf779
Panos Tolios <panostolios@gmail.com>
2020-01-21 21:55:22 +0000

    Make Landing screen responsive for all devices

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,49 @@
 <template>
   <div>
     <v-container class="height100 pa-0" fluid>
-      <div class="d-flex flex-column back fill-height">
-        <div class="app-title d-flex align-center justify-center">
+      <div class="d-flex flex-column back fill-height justify-space-around">
+        <div class="d-flex align-center justify-center height30">
           <app-title>
-            <h1 class="primary--text text-center font-weight-medium">
+            <!-- The bound class makes the text responsive -->
+            <h1
+              class="primary--text text-center font-weight-medium"
+              :class="[$vuetify.breakpoint.smAndUp ? 'display-2' : 'headline']"
+            >
               What will I cook today?
             </h1>
           </app-title>
         </div>
-        <search-form class="px-4" label="Search using keywords" />
-        <div class="pa-4">
-          <div class="text-center">
-            <h2 class="primary--text">- or -</h2>
-          </div>
+        <div class="row flex-grow-0 flex-shrink-1 mt-8">
+          <v-col
+            class="pa-0"
+            sm="10"
+            offset-sm="1"
+            md="8"
+            offset-md="2"
+            lg="6"
+            offset-lg="3"
+            xl="4"
+            offset-xl="4"
+          >
+            <search-form class="px-4 mx-auto" label="Search using keywords" />
+          </v-col>
         </div>
-        <div class="pa-4">
-          <div class="text-center">
-            <app-btn
-              @btnclicked="randomRecipeId"
-              color="primary"
-              class="font-weight-bold"
-              x-large
-              >Surprise Me!</app-btn
-            >
+        <div>
+          <div class="pa-4">
+            <div class="text-center">
+              <h2 class="primary--text">- or -</h2>
+            </div>
+          </div>
+          <div class="pa-4">
+            <div class="text-center">
+              <app-btn
+                @btnclicked="randomRecipeId"
+                color="primary"
+                class="font-weight-bold"
+                x-large
+                >Surprise Me!</app-btn
+              >
+            </div>
           </div>
         </div>
       </div>
@@ -59,24 +79,35 @@ export default {
         const { data } = response;
         const id = data.recipes[0].id;
         router.push({ name: "recipe", params: { id } });
-      })
+      });
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
 v-content {
   margin-top: 0;
   padding-top: 0;
 }
 
 .height100 {
+  /* height: calc(100vh - 56px); */
   height: 100vh;
 }
 
-.app-title {
-  height: 50%;
+.height30 {
+  height: 30%;
+}
+
+.height15 {
+  height: 15%;
+}
+
+/* Override vuetify style */
+>>> h1.display-2,
+h1.headline {
+  font-family: Pacifico, cursive !important;
 }
 
 .back {
