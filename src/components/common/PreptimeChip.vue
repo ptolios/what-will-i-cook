2c39c381e449eecc08e192ac92d75b3f36b69e3b
Panos Tolios <panostolios@gmail.com>
2020-01-31 12:16:15 +0000

    Turn 'preparation time' chip to component

---
+++ src/components/common/PreptimeChip.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-chip class="mt-2 ml-2" v-bind="$attrs">
+    <v-icon class>timer</v-icon>
+    <span class="font-weight-bold px-1" data-test="prep-chip">
+        <slot>-</slot> min
+    </span>
+  </v-chip>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ tests/unit/preptime-chip.spec.ts
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuetify from "vuetify";
+import PreptimeChip from "@/components/common/PreptimeChip.vue";
+
+const localVue = createLocalVue()
+
+describe("PreptimeChip", () => {
+  // let vuetify = new Vuetify;
+  Vue.use(Vuetify);
+
+  
+  it("renders the correct prep time", () => {
+    let wrapper = shallowMount(PreptimeChip, {
+      slots: {
+        default: "45"
+      }
+    })
+    const chipContent = wrapper.find("[data-test='prep-chip']");
+    expect(chipContent.text()).toContain("45 min")
+  })
+
+  it("renders the default prep time", () => {
+    let wrapper = shallowMount(PreptimeChip)
+    const chipContent = wrapper.find("[data-test='prep-chip']");
+    expect(chipContent.text()).toContain("- min")
+  })
+
+});
