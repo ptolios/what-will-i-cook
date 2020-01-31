94672038b440a02e63cc44156967693c32bfb463
Panos Tolios <panostolios@gmail.com>
2020-01-31 13:11:25 +0000

    Pass 'small' attr to v-icon on PreptimeChip

--- src/components/common/PreptimeChip.vue
+++ src/components/common/PreptimeChip.vue
@@ -1,15 +1,24 @@
 <template>
   <v-chip class="mt-2 ml-2" v-bind="$attrs">
-    <v-icon class>timer</v-icon>
+    <v-icon class :small="small">timer</v-icon>
     <span class="font-weight-bold px-1" data-test="prep-chip">
-        <slot>-</slot> min
+      <slot>-</slot> min
     </span>
   </v-chip>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      small: false
+    };
+  },
+  mounted() {
+    if ("small" in this.$attrs) {
+      this.small = this.$attrs["small"];
+    }
+    console.log(this.$attrs);
+  }
+};
 </script>
-
-<style>
-</style>
