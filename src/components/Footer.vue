eb609a15a8b92a2310b14929b99c60b5f38030a3
Panos Tolios <panostolios@gmail.com>
2020-02-11 20:57:36 +0000

    Update Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,18 +3,18 @@
     <v-card class="flex" color="secondary" flat tile>
       <v-card-subtitle class="text-center">
         <div>
-          Powered by
-          <a href="https://spoonacular.com/food-api">Spponacular API</a>
-        </div>
-        <div>
           Created with <a href="https://vuejs.org/">Vue.js 2</a> and
           <a href="https://vuetifyjs.com/">Vuetify 2</a>
+        </div>
+        <div>
+          Using
+          <a href="https://spoonacular.com/food-api">Spoonacular API</a>
         </div>
       </v-card-subtitle>
       <v-divider />
       <v-card-text class="caption py-2 text-center">
         <strong
-          ><a href="https://ptolios.github.io/" target="_blank"
+          ><a href="https://my-resume-cv.netlify.com/" target="_blank"
             >Panos Tolios</a
           ></strong
         >
