a21ea6a8e93f34414a557ebcc7fc7247a2dbce70
root <root@vs-code.servercheap.net>
2020-02-12 10:31:55 +0000

    Remove hamburger & avatar icon from Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,10 @@
 <template>
   <v-app-bar class="white" app :inverted-scroll="invertedScroll">
-    <v-app-bar-nav-icon color="primary"></v-app-bar-nav-icon>
-    <v-spacer />
     <app-title class="ma-auto">
       <h3 class="primary--text align-self-stretch text-truncate">
         <a href="/">What will I cook today?</a>
       </h3>
     </app-title>
-    <v-spacer />
-    <v-btn
-      to="/"
-      class="mr-1"
-      fab
-      icon
-      right
-      v-ripple="{ class: 'primary--text' }"
-    >
-      <v-avatar size="1.71428em">
-        <svg fill="#FFC501" class="mb-1">
-          <use xlink:href="@/assets/meal.svg#Layer_1"></use>
-        </svg>
-      </v-avatar>
-    </v-btn>
   </v-app-bar>
 </template>
 
